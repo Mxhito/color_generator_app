73d95b5955617ada0adf3b3939f45e41f0d3862b
Ilya <56062539+Mok1mo@users.noreply.github.com>
2023-03-09 23:03:08 +0200

    colors history

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -12,6 +12,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final List<ColorHistory> _colorHistory = [];
+
   Color _backgroundColor = Colors.white;
 
   @override
@@ -19,17 +21,27 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Solid Software'),
+        centerTitle: true,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ..._colorHistory.map((history) {
+              return ListTile(
+                title: history.getPreview(),
+              );
+            }).toList(),
+          ],
+        ),
       ),
       body: GestureDetector(
         onTap: () {
           setState(() {
-            const _alpha = 255;
-            const _rgbLimiter = 256;
-            _backgroundColor = Color.fromARGB(
-              _alpha,
-              Random().nextInt(_rgbLimiter),
-              Random().nextInt(_rgbLimiter),
-              Random().nextInt(_rgbLimiter),
+            _backgroundColor = _generateColor();
+            String _hexCode =
+                _backgroundColor.value.toRadixString(16).padLeft(8, '0');
+            _colorHistory.add(
+              ColorHistory(_backgroundColor, _hexCode),
             );
           });
         },
@@ -47,4 +59,53 @@ class _HomeState extends State<Home> {
       ),
     );
   }
+}
+
+/// Model for color history
+class ColorHistory {
+  /// Color
+  final Color color;
+
+  /// HEX of color
+  final String hexCode;
+
+  /// Size of container
+  final double size = 40;
+
+  /// Border radius of container
+  final double bordeRadius = 10;
+
+  /// Model constructor
+  ColorHistory(this.color, this.hexCode);
+
+  /// Widget for display colors
+  Widget getPreview() {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(bordeRadius),
+      ),
+      child: Center(
+        child: Text(
+          hexCode,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
+
+Color _generateColor() {
+  final random = Random();
+  const _alpha = 255;
+  const _rgbLimiter = 256;
+
+  return Color.fromARGB(
+    _alpha,
+    random.nextInt(_rgbLimiter),
+    random.nextInt(_rgbLimiter),
+    random.nextInt(_rgbLimiter),
+  );
 }
